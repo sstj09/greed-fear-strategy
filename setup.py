7458300772d93b3a0e932da6a157351e216fad17
sstj09 <tingjuan1@126.com>
2025-06-02 18:33:37 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='greed-fear-strategy',
+    version='0.1',
+    install_requires=[
+        'matplotlib>=3.7',
+        'pandas>=2.0',
+        'numpy>=1.24',
+        'requests==2.31.0',
+        'scipy==1.13.0',
+        'seaborn==0.13.2',
+        'streamlit>=1.22'
+    ],
+)
